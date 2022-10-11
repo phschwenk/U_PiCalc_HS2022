17fcce394f7993ec0f34abb545d0c848fa13670a
phschwenk <pschwenk@hotmail.de>
2022-10-11 20:53:03 +0200

    Update main.c
    -Eventbit aktualisiert
    -Statemachine angepasst
    -Controllertask geändert Buttonhandler ist neu in einem Seperaten Task
    -Task für berechnen Pi mit euler erstellt

--- U_PiCalc_HS2022/main.c
+++ U_PiCalc_HS2022/main.c
@@ -33,6 +33,8 @@
 
 void controllerTask(void* pvParameters);
 void calculatLeibniz(void* pvParameters);
+void calculatEuler(void* pvParameters);
+void vButtonTask(void *pvParameters);
 float piGerechnet;
 
 //EventGroup for ButtonEvents.
@@ -40,43 +42,85 @@ EventGroupHandle_t egButtonEvents = NULL;
 #define BUTTON1_SHORT	0x01	// Startet den Algorithmus
 #define BUTTON2_SHORT	0x02	// Stoppt den Algorithmus
 #define BUTTON3_SHORT	0x04	// Setzt den Algorithmus zurück
-#define BUTTON4_SHORT	0x08	// Für Zustand und Wechseln vom Algorithmus
-#define BUTTON_ALL		0xFF	// Rücksetzten der event Bit
+#define BUTTON4_SHORT	0x08	// Wechseln vom Algorithmus
+#define START_PI_1		0x10	// Start Stopp Bit für Leibnitz Pi Berechnung
+#define START_PI_2		0x20	// Start Stopp Bit für andere Pi berechnung
+#define PI_READY		0x40	// Rückmeldung sobald Pi Task beendet und auf Stopp ist
+#define PI_GENAU		0x80	// Pi Genauigkeit erreicht
+#define BUTTON_ALL		0xFF	// Rücksetzten der Event Bit
 
 int main(void)
 {
 	vInitClock();
 	vInitDisplay();
 	
-	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
+	xTaskCreate(controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, NULL);
+	xTaskCreate(ButtonTask, (const char *) "Button_task", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL)
 	xTaskCreate(calculatLeibniz, (const char *) "calculat_leibniz", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL)
+	xTaskCreate(calculatEuler, (const char *) "calculat_euler", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL)
 
 	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"PI-Calc HS2022");
-	
+	vDisplayWriteStringAtPos(0,0,"PI-Calc HS2022 Philipp");
 	vTaskStartScheduler();
 	return 0;
 }
 
 //Modes for Finite State Machine
 #define MODE_IDLE 0
-#define MODE_Leibnitz 1
-#define MODE_KommtNoch 2
-#define MODE_ 3
+#define MODE_PI_AUSGEBEN 1
 
+// Steuerungs Task das Display und die sichere ausgabe von Pi macht
 void controllerTask(void* pvParameters) {
-	egButtonEvents = xEventGroupCreate();
-	initButtons();
-	for(;;) {
-		updateButtons();
-		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
-			char pistring[12];
-			sprintf(&pistring[0], "PI: %.8f", piGerechnet);
-			vDisplayWriteStringAtPos(1,0, "%s", pistring);
+	float Pi_Ausgabe = 0;
+	while(egButtonEvents == NULL) { //Wait for EventGroup to be initialized in other task
+		vTaskDelay(10/portTICK_RATE_MS);
 	}
 	for(;;) {
+		switch(mode) {
+			case MODE_IDLE: {
+				vDisplayClear();
+				vDisplayWriteStringAtPos(0,0,"Set Alarm Time");
+				
+				break;	
+			}
+			case MODE_PI_AUSGEBEN{
+				
+				break;
+			}
+		}
+		vTaskDelay(200/portTICK_RATE_MS);
+	}
+}
+
+// Pi berechnen mit der Leibnitz Reihe
+void calculatLeibniz(void* pvParameters){
+	float Pi4_L = 1.0;
+	for(;;){
+		uint32_t n =3;
+		Pi4_L = Pi4_L -(1.0/n)+(1.0/(n+2));
+		n = n+4;
+		piGerechnet = Pi4_L*4;
+	}
+}
+
+// Berechnen von Pi mit der Euler Reihe
+void calculatEuler(void *pvParameters){
+	float PiEuler = 0;
+	for(;;){
+		uint32_t n = 1;
+		PiEuler = PiEuler + (1/(n^2));
+		n++;
+		piGerechnet = sqrt(PiEuler*6);
+	}
+}
+
+// Task liest die Buttons ein uns setzt die eventbits
+void vButtonTask(void *pvParameters) {
+	egButtonEvents = xEventGroupCreate();
+	initButtons(); //Initialize Buttons
+	for(;;) {
 		updateButtons();
-			
+		
 		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
 			xEventGroupSetBits(egButtonEvents, BUTTON1_SHORT);
 		}
@@ -89,15 +133,6 @@ void controllerTask(void* pvParameters) {
 		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
 			xEventGroupSetBits(egButtonEvents, BUTTON4_SHORT);
 		}
-	vTaskDelay(10/portTICK_RATE_MS);
-	}
-}
-void calculatLeibniz(void* pvParameters){
-	float Pi4_L = 1.0;
-	for(;;){
-		uint32_t n =3;
-		Pi4_L = Pi4_L -(1.0/n)+(1.0/(n+2));
-		n = n+4;
-		piGerechnet = Pi4_L*4;
+		vTaskDelay((1000/BUTTON_UPDATE_FREQUENCY_HZ)/portTICK_RATE_MS);
 	}
 }
